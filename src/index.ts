88299fa6f14619dff5408ec91479cdfd38412716
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-12-21 20:39:34 -0700

    Basic route with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hi there");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h2>
+    </div>
+  `)
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
